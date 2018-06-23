b03ce36d09f3cc5225c2ef21522714ee65ed10e7
Tomek Rucinski <tomasz.rucinski@gmail.com>
2018-06-23 22:28:51 +0200

    Initial commit

---
+++ src/main/java/pl/ibm/microservices/microservices/MicroservicesApplication.java
@@ -0,0 +1,11 @@
+package pl.ibm.microservices.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+}
